8552875394ddc798a90464defb0f2726a417fe89
RecursionExcursion <rloup15@gmail.com>
2022-09-20 15:34:24 -0400

    Initial Commit

---
+++ src/main/java/com/foofincworks/MyCalender/MyCalenderApplication.java
@@ -0,0 +1,13 @@
+package com.foofincworks.MyCalender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCalenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCalenderApplication.class, args);
+	}
+
+}
