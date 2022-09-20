dfdfdce82f72358711e9da4f057dfa2a875a2165
RecursionExcursion <rloup15@gmail.com>
2022-09-20 16:18:01 -0400

    Added Event class and set up HomeController. Altered configuration parameters and commented out Spring Security dependency. Lastly, I created a placeholder home.html

---
+++ src/main/java/com/foofincworks/MyCalender/entity/Event.java
@@ -0,0 +1,47 @@
+package com.foofincworks.MyCalender.entity;
+
+public class Event {
+
+    private int id;
+
+    //TODO Make date object?
+    private String date;
+
+    private String eventName;
+    private String eventText;
+
+    public Event() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventText() {
+        return eventText;
+    }
+
+    public void setEventText(String eventText) {
+        this.eventText = eventText;
+    }
+}
