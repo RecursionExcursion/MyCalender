a826eb815140e3aabbec9e49fdd1817a17829b9e
TheFoofers <cklawieter@gmail.com>
2022-09-27 15:51:36 -0400

    added admin rsvp list html

--- src/main/java/com/foofincworks/MyCalender/controller/AdminController.java
+++ src/main/java/com/foofincworks/MyCalender/controller/AdminController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.foofincworks.MyCalender.entity.Event;
 import com.foofincworks.MyCalender.service.mail.EventService;
@@ -28,5 +29,26 @@ public class AdminController {
         List<Event> events = eventService.getAll();
         model.addAttribute("events", events);
         return "admin/admin-home";
+    }
+
+// add event method to get rsvps
+    @GetMapping("/showRSVPList")
+    public String showRSVPList(@RequestParam("eventId") int id, Model model) {
+
+        Event event = null;
+
+        List<Event> events = eventService.getAll();
+
+        for (Event e : events) {
+            if (e.getId() == id) {
+                event = e;
+            }
+        }
+
+        // model.addAttribute("RSVPs", event);
+        model.addAttribute("event", event.getEventName());
+
+        return "admin/rsvp-list";
+
     }
 }
